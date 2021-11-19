3f721e682065e2c9adb773a3ef38f0f3e93bc8c4
Slushie <57730247+slushie0@users.noreply.github.com>
2021-11-19 12:15:25 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Nov 30, 2021 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date();
+  var nowUTC = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - nowUTC;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("days").innerHTML = days + " days";
+  document.getElementById("hrs").innerHTML = hours + " hours";
+  document.getElementById("min").innerHTML = minutes + " minutes";
+  document.getElementById("sec").innerHTML = seconds + " seconds";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
